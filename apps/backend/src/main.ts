964810b1dd842a138407b2c9b5f119755d43cf51
thanhnghic4 <thanhnghic4@gmail.com>
2023-11-23 16:54:58 +0700

    init-frontend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule.register('postgres'));
-  await app.listen(3000);
+  await app.listen(2222);
 }
 bootstrap();
