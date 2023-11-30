d3f9d55f6d202fbdbfcd96a29fdba76fdf669774
thanhnghi <thanhnghic4@gmail.com>
2023-11-30 10:51:16 +0000

    setup pre-command

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule.register('postgres'));
+  const app = await NestFactory.create(AppModule.register('xgame'));
   await app.listen(2223);
 }
 bootstrap();
---
+++ setup_dev.sh
@@ -0,0 +1,2 @@
+npm i -g turbo
+pnpm config set enable-pre-post-scripts true
